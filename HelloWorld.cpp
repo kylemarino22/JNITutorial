6f5792daa8268c05f17a75f419381fcae744ebbb
Unknown <kyle.marino22@gmail.com>
2018-02-17 23:35:27 -0800

    Adding Readme and Example files

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <iostream>
+#include "HelloWorld.h"
+using namespace std;
+ 
+JNIEXPORT void JNICALL 
+Java_HelloWorld_print(JNIEnv *, jobject){
+    cout << "Hello asdfmaskd!\n";
+    return;
+}
